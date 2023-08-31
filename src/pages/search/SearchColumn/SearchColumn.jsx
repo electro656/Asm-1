d4b4f3f8ad08a2b36fce8c08ea78340f7ac7bca2
electro656 <106375906+electro656@users.noreply.github.com>
2023-08-31 14:47:10 +0700

    update 1 for search page

--- src/pages/search/SearchColumn/SearchColumn.jsx
+++ src/pages/search/SearchColumn/SearchColumn.jsx
@@ -1,15 +1,53 @@
 import styles from "./SearchColumn.module.css";
 
 export default function SearchColumn() {
+  function nothing(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className={styles["search-column"]}>
-      <h3>Search</h3>
-      <form className={styles["search-form"]}>
+      <h2>Search</h2>
+      <form onSubmit={nothing} className={styles["search-form"]}>
         <label>Destination</label>
         <input />
 
         <label>Check-in date</label>
-        <input />
+        <input value="06/24/2022 to 06/24/2022" />
+
+        <label>Options</label>
+        <div className={styles["search-options"]}>
+          <div>
+            <label>
+              Min prices <span>per night</span>
+            </label>
+            <input />
+          </div>
+
+          <div>
+            <label>
+              Max prices <span>per night</span>
+            </label>
+            <input />
+          </div>
+
+          <div>
+            <label>Adult</label>
+            <input placeholder="1" />
+          </div>
+
+          <div>
+            <label>Children</label>
+            <input placeholder="0" />
+          </div>
+
+          <div>
+            <label>Room</label>
+            <input placeholder="1" />
+          </div>
+        </div>
+
+        <button type="submit">Search</button>
       </form>
     </div>
   );
